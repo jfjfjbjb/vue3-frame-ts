0622b89cde5c849f816a94069ec286a0beb0259b
jfjfjbjb <697451yr@163.com>
2022-09-23 17:22:07 +0800

    init

---
+++ .eslintrc.cjs
@@ -0,0 +1,35 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution');
+
+module.exports = {
+  root: true,
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript'
+    // '@vue/eslint-config-prettier'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  rules: {
+    'quotes': ['error', 'single'],
+    // 'quote-props': ['error', 'as-needed'],
+    'jsx-quotes': ['error', 'prefer-single'],
+    'no-unused-vars': ['error', { args: 'none' }],
+    'vue/multi-word-component-names': 0
+  },
+  globals: {
+    require: 'writable',
+    defineOptions: 'readonly',
+    _: 'readonly',
+    $config: 'readonly',
+    $bus: 'readonly',
+    $loading: 'readonly',
+    $common: 'readonly',
+    $message: 'readonly',
+    $notification: 'readonly',
+    $ROOT: 'readonly',
+    $ENTRY: 'readonly'
+  }
+};
